32ee8bc39d478645b2b287a92962ab5ba72f3b33
knoten <philipp.schweiger85@googlemail.com>
2017-09-16 15:44:09 +0200

    Farben eingefügt

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -1,4 +1,34 @@
 package com.sabel;
 
-public class Farbenspiel {
+import javax.swing.*;
+import java.awt.*;
+
+public class Farbenspiel extends JFrame {
+
+
+    private JPanel jpnl;
+
+    public Farbenspiel() {
+        this.setTitle("Farbenspiel");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300, 300);
+        initComponents();
+        this.setVisible(true);
+    }
+
+    private void initComponents(){
+        JButton jBtnGruen = new JButton("Gruen");
+        JButton jBtnBlau = new JButton("Blau");
+        JButton jBtnGelb = new JButton("Gelb");
+        jpnl = new JPanel();
+        jBtnBlau.setBackground(Color.blue);
+        jBtnGelb.setBackground(Color.yellow);
+        jBtnGruen.setBackground(Color.green);
+        jpnl.add(jBtnGruen);
+        jpnl.add(jBtnGelb);
+        jpnl.add(jBtnBlau);
+        this.add(jpnl);
+    }
+
+
 }
