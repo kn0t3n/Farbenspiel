3187e3943683a0d62b6eeafa9572e0759d610036
knoten85 <philipp.schweiger85@googlemail.com>
2017-09-18 10:27:15 +0200

    initial commit

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -2,8 +2,10 @@ package com.sabel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Farbenspiel extends JFrame {
+public class Farbenspiel extends JFrame implements ActionListener {
 
 
     private JPanel jpnl;
@@ -16,7 +18,7 @@ public class Farbenspiel extends JFrame {
         this.setVisible(true);
     }
 
-    private void initComponents(){
+    private void initComponents() {
         JButton jBtnGruen = new JButton("Gruen");
         JButton jBtnBlau = new JButton("Blau");
         JButton jBtnGelb = new JButton("Gelb");
@@ -24,6 +26,9 @@ public class Farbenspiel extends JFrame {
         jBtnBlau.setBackground(Color.blue);
         jBtnGelb.setBackground(Color.yellow);
         jBtnGruen.setBackground(Color.green);
+        jBtnBlau.addActionListener(this);
+        jBtnGelb.addActionListener(this);
+        jBtnGruen.addActionListener(this);
         jpnl.add(jBtnGruen);
         jpnl.add(jBtnGelb);
         jpnl.add(jBtnBlau);
@@ -31,4 +36,18 @@ public class Farbenspiel extends JFrame {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+        if (e.getActionCommand() == "Blau") {
+            jpnl.setBackground(Color.blue);
+        }
+        if (e.getActionCommand()=="Gelb"){
+            jpnl.setBackground(Color.yellow);
+        }
+        if (e.getActionCommand()=="Gruen"){
+            jpnl.setBackground(Color.green);
+        }
+        repaint();
+    }
 }
