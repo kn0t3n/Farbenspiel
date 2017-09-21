195b9237e11270ca15c9f09bc1737d91624bd237
knoten85 <philipp.schweiger85@googlemail.com>
2017-09-21 08:59:44 +0200

    Events implemented with switch

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -7,46 +7,63 @@ import java.awt.event.ActionListener;
 
 public class Farbenspiel extends JFrame implements ActionListener {
 
-
     private JPanel jpnl;
+    private JButton jBtnGruen;
+    private JButton jBtnBlau;
+    private JButton jBtnGelb;
 
     public Farbenspiel() {
+
         this.setTitle("Farbenspiel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 300);
-        initComponents();
+
+        this.initComponents();
+        this.initEvents();
+
         this.setVisible(true);
     }
 
-    private void initComponents() {
-        JButton jBtnGruen = new JButton("Gruen");
-        JButton jBtnBlau = new JButton("Blau");
-        JButton jBtnGelb = new JButton("Gelb");
-        jpnl = new JPanel();
-        jBtnBlau.setBackground(Color.blue);
-        jBtnGelb.setBackground(Color.yellow);
-        jBtnGruen.setBackground(Color.green);
+    private void initEvents() {
         jBtnBlau.addActionListener(this);
         jBtnGelb.addActionListener(this);
         jBtnGruen.addActionListener(this);
+    }
+
+    private void initComponents() {
+
+        jBtnGruen = new JButton("Gruen");
+        jBtnBlau = new JButton("Blau");
+        jBtnGelb = new JButton("Gelb");
+
+        jpnl = new JPanel();
+
+        jBtnBlau.setBackground(Color.blue);
+        jBtnGelb.setBackground(Color.yellow);
+        jBtnGruen.setBackground(Color.green);
+
         jpnl.add(jBtnGruen);
         jpnl.add(jBtnGelb);
         jpnl.add(jBtnBlau);
+
         this.add(jpnl);
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
+
         System.out.println(e.getActionCommand());
-        if (e.getActionCommand() == "Blau") {
-            jpnl.setBackground(Color.blue);
-        }
-        if (e.getActionCommand()=="Gelb"){
-            jpnl.setBackground(Color.yellow);
-        }
-        if (e.getActionCommand()=="Gruen"){
-            jpnl.setBackground(Color.green);
+
+        switch (e.getActionCommand()) {
+            case "Gruen":
+                jpnl.setBackground(Color.green);
+                break;
+            case "Blau":
+                jpnl.setBackground(Color.blue);
+                break;
+            case "Gelb":
+                jpnl.setBackground(Color.yellow);
+                break;
         }
         repaint();
     }
